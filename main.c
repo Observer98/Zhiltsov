fd548cf89a0ec6ce8862e8dc4d527b8e4efa58d4
Alex <a.zhiltzov2016@yandex.ru>
2021-09-29 15:27:34 +0300

    give hello

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
@@ -12,5 +13,3 @@ int main(int argc, char **argv)
     printf("Hello %s!\n", name);
     return 0;
 }
-
-	
